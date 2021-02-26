bacb8b05ef80511d6212cd334f4b7f5ae93ea1db
Victor Cardoso Lima <victor.lima@atlasinovacoes.com.br>
2021-02-26 20:03:02 -0300

    Created application components

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,27 +1,23 @@
-import { useState, useEffect } from "react";
+import React, { useContext, Fragment } from "react";
+import { CountdownContext } from "../contexts/CountdownContext";
 import styles from "../styles/components/Countdown.module.scss";
 
 export function Countdown() {
-    const [time, setTime] = useState(25 * 60);
-    const [active, setActive] = useState(false);
+    const { 
+        minutes,
+        seconds,
+        hasFinished,
+        isActive,
+        startCountdown,
+        resetCountdown,
+    } = useContext(CountdownContext);
 
-    const minutes = Math.floor(time / 60);
-    const seconds = time % 60;
-
-    const [minuteLeft, minuteRight] = String(minutes).padStart(2, "0").split("");
-    const [secondLeft, secondRight] = String(seconds).padStart(2, "0").split("");
-
-    function startCountdown() {
-        setActive(true);
-    }
-
-    useEffect(() => {
-        if (active && time > 0) {
-            setTimeout(() => {
-                setTime(time - 1);
-            }, 1000)
-        }
-    }, [active, time])
+    const [minuteLeft, minuteRight] = String(minutes)
+        .padStart(2, "0")
+        .split("");
+    const [secondLeft, secondRight] = String(seconds)
+        .padStart(2, "0")
+        .split("");
 
     return (
         <div>
@@ -36,13 +32,32 @@ export function Countdown() {
                     <span>{secondRight}</span>
                 </div>
             </div>
-            <button
-                type="button"
-                className={styles.countdownButton}
-                onClick={startCountdown}
-            >
-                Iniciar um ciclo
-            </button>
+            {hasFinished ? (
+                <button disabled className={styles.countdownButton}>
+                    Ciclo encerrado
+                    <img src="icons/check.svg" alt="Concluded" />
+                </button>
+            ) : (
+                <Fragment>
+                    {isActive ? (
+                        <button
+                            type="button"
+                            className={`${styles.countdownButton} ${styles.isActive}`}
+                            onClick={resetCountdown}
+                        >
+                            Abandonar ciclo
+                        </button>
+                    ) : (
+                        <button
+                            type="button"
+                            className={styles.countdownButton}
+                            onClick={startCountdown}
+                        >
+                            Iniciar um ciclo
+                        </button>
+                    )}
+                </Fragment>
+            )}
         </div>
     );
 }
--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,14 +1,19 @@
+import { useContext } from "react";
+import { ChallengesContext } from "../contexts/ChallengeContext";
+
 import styles from "../styles/components/Profile.module.scss";
 
 export function Profile() {
+    const { level } = useContext(ChallengesContext);
+
     return (
         <div className={styles.profileContainer}>
             <img src="http://github.com/vctrlima.png" alt="Victor Lima" />
             <div>
                 <strong>Victor Lima</strong>
                 <p>
-                    <img src="icons/level.svg" alt="Level"/>
-                    Level 1
+                    <img src="icons/level.svg" alt="Level" />
+                    Level {level}
                 </p>
             </div>
         </div>
